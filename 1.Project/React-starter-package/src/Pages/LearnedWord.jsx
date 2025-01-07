df97bb4ff9846a98e59c5873e56b3743b974ebcc
Parmanand-Parveen <parmanandparveen@gmail.com>
2025-01-07 11:57:09 +0530

    fix: update import paths for FlashCard component to remove file extension

--- 1.Project/React-starter-package/src/Pages/LearnedWord.jsx
+++ 1.Project/React-starter-package/src/Pages/LearnedWord.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import FlashCard from "../Components/FlashCard.jsx";
+import FlashCard from "../Components/FlashCard";
+
 
 
 
