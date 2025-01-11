88696c1911fecf0a2dd0f75bc190bf5707115396
Parmanand-Parveen <parmanandparveen@gmail.com>
2025-01-12 00:18:23 +0530

    Store removed

--- 2.Project/Voiceapp/src/main.jsx
+++ 2.Project/Voiceapp/src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
-import store from "./Store/Store.js";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}>
+    
       <App />
-    </Provider>
+ 
   </BrowserRouter>
 );
